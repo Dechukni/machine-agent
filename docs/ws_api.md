c517493108332490b717cba0a9f8de1e7e1cc782
Yevhenii Voevodin <targetjump@gmail.com>
2016-08-11 02:39:55 +0300

    Send results of process.* operation executions :arrow_up: docs

--- docs/ws_api.md
+++ docs/ws_api.md
@@ -1,8 +1,62 @@
-
-Process API
+Websocket API
 ---
+A message from a client to a server called _operation call_.
+Each operation call must contain at least operation name, and may contain identifier and call body.
+The example of the operation call.
 
-### Start process
+```json
+{
+    "operation" : "process.start",
+    "id" : "id1234567",
+    "body" : {
+        "name" : "build_no_tests",
+        "commandLine" : "mvn clean install -Dmaven.test.skip",
+        "type" : "maven"
+    }
+}
+```
+
+- __operation__ - the name of the operation, usually dot separated resource and action
+- __id__ - is an optional parameter, if client sends it then it is guaranteed 
+that the result of the operation execution will contain the same identifier
+- __body__ - operation execution body, data needed to perform operation
+
+The example of the operation execution result:
+
+```json
+{
+    "id" : "id1234567",
+    "body" : {
+        "pid" : 1,
+        "name" : "build_no_tests",
+        "commandLine" : "mvn clean install -Dmaven.test.skip",
+        "type" : "maven"
+    },
+    "error" : null
+}
+```
+
+The __error__ and __body__ are mutual exclusive, if the operation
+can't be executed due to an error then the response will contain the error 
+message and the error code. The example of the operation response with error:
+
+```json
+{
+    "id" : 12345,
+    "body" : null,
+    "error" : {
+        "code" : 10002,
+        "message" : "Command line required"
+    }
+}
+```
+
+
+### Process API
+
+#### Start process
+
+##### Call
 
 - __name__ - the name of the command
 - __commandLine__ - command line to execute
@@ -11,9 +65,9 @@ Process API
  received by this channel. By default all the process events will be received.
 
 ```json
-
 {
     "operation" : "process.start",
+    "id" : "0x12345",
     "body" : {
         "name" : "build",
         "commandLine" : "mvn clean install",
@@ -23,30 +77,64 @@ Process API
 }
 ```
 
-### Kill process
+##### Result
+
+```json
+{
+    "id" : "0x12345",
+    "body" : {
+        "pid" : 123,
+        "name" : "build",
+        "commandLine" : "mvn clean install",
+        "type" : "maven"
+    },
+    "error" : null
+}
+```
+
+#### Kill process
+
+##### Call
 
 - __pid__ - the id of the process to kill
 
 ```json
 {
     "operation" : "process.kill",
+    "id" : "0x12345",
     "body" : {
         "pid" : 123
     }
 }
 ```
 
-### Subscribe to process events
+##### Result
+
+```json
+{
+    "id" : "0x12345",
+    "body" : {
+        "pid" : 123,
+        "text" : "Successfully killed"
+    },
+    "error" : null
+}
+```
+
+#### Subscribe to process events
+
+##### Call
 
 - __pid__ - the id of the process to subscribe to
 - __eventTypes__(optional) - comma separated types of events which will be
-received by this channel. By default all the process events will be received.
+received by this channel. By default all the process events will be received
 - __after__(optional) - process logs which appeared after given time will
 be republished to the channel. This parameter may be useful when reconnecting to the machine-agent
 
 ```json
 {
     "operation" : "process.subscribe",
+    "id" : "0x12345",
     "body" : {
         "pid" : 123,
         "eventTypes" : "stdout,stderr",
@@ -55,20 +143,52 @@ be republished to the channel. This parameter may be useful when reconnecting to
 }
 ```
 
-### Unsubscribe from process events
+##### Result
+
+```json
+{
+    "id" : "0x12345",
+    "body" : {
+        "pid" : 123,
+        "eventTypes" : "stdout,stderr",
+        "text" : "Successfully subscribed"
+    },
+    "error" : null
+}
+```
+
+
+#### Unsubscribe from process events
+
+##### Call
 
 - __pid__ - the id of the process to unsubscribe from
 
 ```json
 {
     "operation" : "process.unsubscribe",
+    "id" : "0x12345",
     "body" : {
         "pid" : 123
     }
 }
 ```
 
-### Update process subscriber
+##### Result
+
+```json
+{
+    "id" : "0x12345",
+    "body" : {
+        "pid" : 123,
+        "text" : "Successfully unsubscribed"
+    }
+}
+```
+
+#### Update process subscriber
+
+##### Call
 
 - __pid__ - the id of the process which subscriber should be updated
 - __eventTypes__ - comma separated types of events which will be
@@ -77,9 +197,23 @@ received by this channel.
 ```json
 {
     "operation" : "process.updateSubscriber",
+    "id" : "0x12345",
     "body" : {
         "pid" : 123,
         "eventTypes": "process_status,stderr"
+    }
+}
+```
+
+##### Result
+
+```json
+{
+    "id" : "0x12345",
+    "body" : {
+        "pid" : 123,
+        "eventTypes": "process_status,stderr",
+        "text" : "Subscriber successfully updated"
     }
 }
 ```
--- process/ws_service.go
+++ process/ws_service.go
@@ -24,86 +24,97 @@ var OpRoutes = op.RoutesGroup{
 		{
 			ProcessStartOp,
 			func(body []byte) (interface{}, error) {
-				b := StartProcessCallBody{}
+				b := startBody{}
 				err := json.Unmarshal(body, &b)
 				return b, err
 			},
-			StartProcessCallHF,
+			startProcessHF,
 		},
 		{
 			ProcessKillOp,
 			func(body []byte) (interface{}, error) {
-				b := KillProcessCallBody{}
+				b := killBody{}
 				err := json.Unmarshal(body, &b)
 				return b, err
 			},
-			KillProcessCallHF,
+			killProcessHF,
 		},
 		{
 			ProcessSubscribeOp,
 			func(body []byte) (interface{}, error) {
-				b := SubscribeToProcessCallBody{}
+				b := subscribeBody{}
 				err := json.Unmarshal(body, &b)
 				return b, err
 			},
-			SubscribeToProcessCallHF,
+			subscribeHF,
 		},
 		{
 			ProcessUnsubscribeOp,
 			func(body []byte) (interface{}, error) {
-				b := UnsubscribeFromProcessCallBody{}
+				b := unsubscribeBody{}
 				err := json.Unmarshal(body, &b)
 				return b, err
 			},
-			UnsubscribeFromProcessCallHF,
+			unsubscribeHF,
 		},
 		{
 			ProcessUpdateSubscriberOp,
 			func(body []byte) (interface{}, error) {
-				b := UpdateProcessSubscriberCallBody{}
+				b := updateSubscriberBody{}
 				err := json.Unmarshal(body, &b)
 				return b, err
 			},
-			UpdateProcessSubscriberCallHF,
+			updateSubscriberHF,
 		},
 	},
 }
 
-type StartProcessCallBody struct {
+type startBody struct {
 	Name        string `json:"name"`
 	CommandLine string `json:"commandLine"`
 	Type        string `json:"type"`
 	EventTypes  string `json:"eventTypes"`
 }
 
-type KillProcessCallBody struct {
+type killBody struct {
 	Pid       uint64 `json:"pid"`
 	NativePid uint64 `json:"nativePid"`
 }
 
-type SubscribeToProcessCallBody struct {
+type subscribeBody struct {
 	Pid        uint64 `json:"pid"`
 	EventTypes string `json:"eventTypes"`
 	After      string `json:"after"`
 }
 
-type UnsubscribeFromProcessCallBody struct {
+type subscribeResult struct {
+	Pid        uint64 `json:"pid"`
+	EventTypes string `json:"eventTypes"`
+	Text       string `json:"text"`
+}
+
+type unsubscribeBody struct {
 	Pid uint64 `json:"pid"`
 }
 
-type UpdateProcessSubscriberCallBody struct {
+type updateSubscriberBody struct {
 	Pid        uint64 `json:"pid"`
 	EventTypes string `json:"eventTypes"`
 }
 
-func StartProcessCallHF(body interface{}, t op.Transmitter) error {
-	startCall := body.(StartProcessCallBody)
+type processOpResult struct {
+	Pid  uint64 `json:"pid"`
+	Text string `json:"text"`
+}
+
+func startProcessHF(body interface{}, t op.Transmitter) error {
+	startBody := body.(startBody)
 
 	// Creating command
 	command := Command{
-		Name:        startCall.Name,
-		CommandLine: startCall.CommandLine,
-		Type:        startCall.Type,
+		Name:        startBody.Name,
+		CommandLine: startBody.CommandLine,
+		Type:        startBody.Type,
 	}
 	if err := checkCommand(&command); err != nil {
 		return op.NewArgsError(err)
@@ -112,11 +123,13 @@ func StartProcessCallHF(body interface{}, t op.Transmitter) error {
 	// Detecting subscription mask
 	subscriber := &Subscriber{
 		Id:      t.Channel().Id,
-		Mask:    parseTypes(startCall.EventTypes),
+		Mask:    parseTypes(startBody.EventTypes),
 		Channel: t.Channel().Events,
 	}
 
-	process := NewProcess(command).BeforeEventsHook(func(process *MachineProcess) { t.Send(process) })
+	process := NewProcess(command).BeforeEventsHook(func(process *MachineProcess) {
+		t.Send(process)
+	})
 	if subscriber != nil {
 		if err := process.AddSubscriber(subscriber); err != nil {
 			return err
@@ -126,17 +139,24 @@ func StartProcessCallHF(body interface{}, t op.Transmitter) error {
 	return process.Start()
 }
 
-func KillProcessCallHF(body interface{}, t op.Transmitter) error {
-	killBody := body.(KillProcessCallBody)
+func killProcessHF(body interface{}, t op.Transmitter) error {
+	killBody := body.(killBody)
 	p, ok := Get(killBody.Pid)
 	if !ok {
 		return newNoSuchProcessError(killBody.Pid)
 	}
-	return p.Kill()
+	if err := p.Kill(); err != nil {
+		return err
+	}
+	t.Send(&processOpResult{
+		Pid:  killBody.Pid,
+		Text: "Successfully killed",
+	})
+	return nil
 }
 
-func SubscribeToProcessCallHF(body interface{}, t op.Transmitter) error {
-	subscribeBody := body.(SubscribeToProcessCallBody)
+func subscribeHF(body interface{}, t op.Transmitter) error {
+	subscribeBody := body.(subscribeBody)
 	p, ok := Get(subscribeBody.Pid)
 	if !ok {
 		return newNoSuchProcessError(subscribeBody.Pid)
@@ -157,22 +177,33 @@ func SubscribeToProcessCallHF(body interface{}, t op.Transmitter) error {
 	if err != nil {
 		return op.NewArgsError(errors.New("Bad format of 'after', " + err.Error()))
 	}
-	return p.RestoreSubscriber(subscriber, after)
+	if err := p.RestoreSubscriber(subscriber, after); err != nil {
+		return err
+	}
+	t.Send(&subscribeResult{
+		Pid:        p.Pid,
+		EventTypes: subscribeBody.EventTypes,
+		Text:       "Successfully unsubscribed",
+	})
+	return nil
 }
 
-func UnsubscribeFromProcessCallHF(call interface{}, t op.Transmitter) error {
-	ubsubscribeBody := call.(UnsubscribeFromProcessCallBody)
+func unsubscribeHF(call interface{}, t op.Transmitter) error {
+	ubsubscribeBody := call.(unsubscribeBody)
 	p, ok := Get(ubsubscribeBody.Pid)
 	if !ok {
 		return errors.New(fmt.Sprintf("Process with id '%s' doesn't exist", ubsubscribeBody.Pid))
 	}
-
 	p.RemoveSubscriber(t.Channel().Id)
+	t.Send(&processOpResult{
+		Pid:  p.Pid,
+		Text: "Successfully unsubscribed",
+	})
 	return nil
 }
 
-func UpdateProcessSubscriberCallHF(body interface{}, t op.Transmitter) error {
-	updateBody := body.(UpdateProcessSubscriberCallBody)
+func updateSubscriberHF(body interface{}, t op.Transmitter) error {
+	updateBody := body.(updateSubscriberBody)
 	p, ok := Get(updateBody.Pid)
 	if !ok {
 		return newNoSuchProcessError(updateBody.Pid)
@@ -181,7 +212,14 @@ func UpdateProcessSubscriberCallHF(body interface{}, t op.Transmitter) error {
 		return op.NewArgsError(errors.New("'eventTypes' required for subscriber update"))
 	}
 
-	p.UpdateSubscriber(t.Channel().Id, maskFromTypes(updateBody.EventTypes))
+	if err := p.UpdateSubscriber(t.Channel().Id, maskFromTypes(updateBody.EventTypes)); err != nil {
+		return err
+	}
+	t.Send(&subscribeResult{
+		Pid:        p.Pid,
+		EventTypes: updateBody.EventTypes,
+		Text:       "Subscriber successfully updated",
+	})
 	return nil
 }
 
